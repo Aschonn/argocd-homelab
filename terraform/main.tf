09943e43ae22f6bfa751864dc3384a8b401e7cf3
aschonn <andrew.schonnesen@gmail.com>
2026-01-21 20:08:42 +0000

    updating repo structure and adding userfriendly scripts

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,22 +1,22 @@
-resource "helm_release" "cilium" {
-  name       = "cilium"
-  repository = "https://helm.cilium.io/"
-  chart      = "cilium"
-  namespace  = "kube-system"
-  version    = "1.16.5"
+# resource "helm_release" "cilium" {
+#   name       = "cilium"
+#   repository = "https://helm.cilium.io/"
+#   chart      = "cilium"
+#   namespace  = "kube-system"
+#   version    = "1.16.5"
 
-  values = [
-    file("values/cilium.yaml")
-  ]
-}
+#   values = [
+#     file("../infra/networking/cilium/values.yaml")
+#   ]
+# }
 
-resource "helm_release" "argocd" {
-    name = "argocd"
-    repository = "https://argoproj.github.io/argo-helm"
-    chart = "argo-cd"
-    namespace = "argocd"   
-    create_namespace = true
-    values = [
-    file("values/argocd.yaml")
-    ]
-}
+# resource "helm_release" "argocd" {
+#     name = "argocd"
+#     repository = "https://argoproj.github.io/argo-helm"
+#     chart = "argo-cd"
+#     namespace = "argocd"   
+#     create_namespace = true
+#     values = [
+#     file("../infra/networking/cilium/values.yaml ")
+#     ]
+# }
